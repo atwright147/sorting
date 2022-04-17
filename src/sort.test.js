5ff36ddde41cde9c2b2b734ddcdf9a8f9ed7f5ca
Andy Wright <atwright147@gmail.com>
2022-04-17 16:38:35 +0100

    add better tests

--- src/sort.test.js
+++ src/sort.test.js
@@ -6,153 +6,78 @@ describe('sort', () => {
 
     beforeEach(() => {
       mockData = [
-        {
-          category: 'Electronics',
-          itemName: 'TV',
-          clicks: '12',
-          quantity: '1',
-          amount: '300'
-        },
-        {
-          category: 'Household',
-          itemName: '$total',
-          clicks: '60',
-          quantity: '12',
-          amount: '37'
-        },
-        {
-          category: 'Electronics',
-          itemName: '$total',
-          clicks: '100',
-          quantity: '6',
-          amount: '665'
-        },
-        {
-          category: 'Household',
-          itemName: 'Fairy dish liquid',
-          clicks: '44',
-          quantity: '5',
-          amount: '10'
-        },
-        {
-          category: 'Electronics',
-          itemName: 'Speakers',
-          clicks: '43',
-          quantity: '4',
-          amount: '240'
-        },
-        {
-          category: '$total',
-          itemName: '$total',
-          clicks: '160',
-          quantity: '18',
-          amount: '702'
-        },
-        {
-          category: 'Electronics',
-          itemName: 'Microwave',
-          clicks: '4',
-          quantity: '1',
-          amount: '85'
-        },
-        {
-          category: 'Household',
-          itemName: 'Toothbrush',
-          clicks: '4',
-          quantity: '4',
-          amount: '12'
-        },
-        {
-          category: 'Household',
-          itemName: 'Hand wash',
-          clicks: '12',
-          quantity: '3',
-          amount: '15'
-        },
-        {
-          category: 'Electronics',
-          itemName: 'Toaster',
-          clicks: '41',
-          quantity: '1',
-          amount: '40'
-        }
-      ]
+        { "category": "Electronics", "itemName": "TV", "clicks": 12, "quantity": 1, "amount": 300 },
+        { "category": "Household", "itemName": "$total", "clicks": 60, "quantity": 12, "amount": 37 },
+        { "category": "Electronics", "itemName": "$total", "clicks": 100, "quantity": 6, "amount": 665 },
+        { "category": "Household", "itemName": "Fairy dish liquid", "clicks": 44, "quantity": 5, "amount": 10 },
+        { "category": "Electronics", "itemName": "Speakers", "clicks": 43, "quantity": 3, "amount": 240 },
+        { "category": "$total", "itemName": "$total", "clicks": 160, "quantity": 18, "amount": 702 },
+        { "category": "Electronics", "itemName": "Microwave", "clicks": 4, "quantity": 1, "amount": 85 },
+        { "category": "Household", "itemName": "Toothbrush", "clicks": 4, "quantity": 4, "amount": 12 },
+        { "category": "Household", "itemName": "Hand wash", "clicks": 12, "quantity": 3, "amount": 15 },
+        { "category": "Electronics", "itemName": "Toaster", "clicks": 41, "quantity": 1, "amount": 40 },
+      ];
     });
 
-    it('should sort the data in a hierarchical order', () => {
-      const expected = [
-        {
-          amount: '702',
-          category: '$total',
-          clicks: '160',
-          itemName: '$total',
-          quantity: '18'
-        },
-        {
-          amount: '665',
-          category: 'Electronics',
-          clicks: '100',
-          itemName: '$total',
-          quantity: '6'
-        },
-        {
-          amount: '85',
-          category: 'Electronics',
-          clicks: '4',
-          itemName: 'Microwave',
-          quantity: '1'
-        },
-        {
-          amount: '240',
-          category: 'Electronics',
-          clicks: '43',
-          itemName: 'Speakers',
-          quantity: '4'
-        },
-        {
-          amount: '40',
-          category: 'Electronics',
-          clicks: '41',
-          itemName: 'Toaster',
-          quantity: '1'
-        },
-        {
-          amount: '300',
-          category: 'Electronics',
-          clicks: '12',
-          itemName: 'TV',
-          quantity: '1'
-        },
-        {
-          amount: '37',
-          category: 'Household',
-          clicks: '60',
-          itemName: '$total',
-          quantity: '12'
-        },
-        {
-          amount: '10',
-          category: 'Household',
-          clicks: '44',
-          itemName: 'Fairy dish liquid',
-          quantity: '5'
-        },
-        {
-          amount: '15',
-          category: 'Household',
-          clicks: '12',
-          itemName: 'Hand wash',
-          quantity: '3'
-        },
-        {
-          amount: '12',
-          category: 'Household',
-          clicks: '4',
-          itemName: 'Toothbrush',
-          quantity: '4'
-        }
-      ];
-      expect(sortData(mockData)).toEqual(expected);
+    fdescribe('given sort key is "clicks"', () => {
+      it('should sort the data by "clicks"', () => {
+        const expected = [
+          { "category": "$total", "itemName": "$total", "clicks": 160, "quantity": 18, "amount": 702 },
+          { "category": "Electronics", "itemName": "$total", "clicks": 100, "quantity": 6, "amount": 665 },
+          { "category": "Electronics", "itemName": "Speakers", "clicks": 43, "quantity": 3, "amount": 240 },
+          { "category": "Electronics", "itemName": "Toaster", "clicks": 41, "quantity": 1, "amount": 40 },
+          { "category": "Electronics", "itemName": "TV", "clicks": 12, "quantity": 1, "amount": 300 },
+          { "category": "Electronics", "itemName": "Microwave", "clicks": 4, "quantity": 1, "amount": 85 },
+          { "category": "Household", "itemName": "$total", "clicks": 60, "quantity": 12, "amount": 37 },
+          { "category": "Household", "itemName": "Fairy dish liquid", "clicks": 44, "quantity": 5, "amount": 10 },
+          { "category": "Household", "itemName": "Hand wash", "clicks": 12, "quantity": 3, "amount": 15 },
+          { "category": "Household", "itemName": "Toothbrush", "clicks": 4, "quantity": 4, "amount": 12 },
+        ];
+
+        console.info('ACTUAL');
+        console.table(sortData(mockData, 'clicks'));
+        console.info('EXPECTED');
+        console.table(expected);
+        expect(sortData(mockData, 'clicks')).toEqual(expected);
+      });
+    });
+
+    describe('given sort key is "quantity"', () => {
+      it('should sort the data by "quantity"', () => {
+        const expected = [
+          { "category": "$total", "itemName": "$total", "clicks": 160, "quantity": 18, "amount": 702 },
+          { "category": "Household", "itemName": "$total", "clicks": 60, "quantity": 12, "amount": 37 },
+          { "category": "Household", "itemName": "Fairy dish liquid", "clicks": 44, "quantity": 5, "amount": 10 },
+          { "category": "Household", "itemName": "Toothbrush", "clicks": 4, "quantity": 4, "amount": 12 },
+          { "category": "Household", "itemName": "Hand wash", "clicks": 12, "quantity": 3, "amount": 15 },
+          { "category": "Electronics", "itemName": "$total", "clicks": 100, "quantity": 6, "amount": 665 },
+          { "category": "Electronics", "itemName": "Speakers", "clicks": 43, "quantity": 3, "amount": 240 },
+          { "category": "Electronics", "itemName": "TV", "clicks": 12, "quantity": 1, "amount": 300 },
+          { "category": "Electronics", "itemName": "Microwave", "clicks": 4, "quantity": 1, "amount": 85 },
+          { "category": "Electronics", "itemName": "Toaster", "clicks": 41, "quantity": 1, "amount": 40 },
+         ];
+
+        expect(sortData(mockData, 'quantity')).toEqual(expected);
+      });
+    });
+
+    describe('given sort key is "amount"', () => {
+      it('should sort the data by "amount"', () => {
+        const expected = [
+          { "category": "$total", "itemName": "$total", "clicks": 160, "quantity": 18, "amount": 702 },
+          { "category": "Electronics", "itemName": "$total", "clicks": 100, "quantity": 6, "amount": 665 },
+          { "category": "Electronics", "itemName": "TV", "clicks": 12, "quantity": 1, "amount": 300 },
+          { "category": "Electronics", "itemName": "Speakers", "clicks": 43, "quantity": 3, "amount": 240 },
+          { "category": "Electronics", "itemName": "Microwave", "clicks": 4, "quantity": 1, "amount": 85 },
+          { "category": "Electronics", "itemName": "Toaster", "clicks": 41, "quantity": 1, "amount": 40 },
+          { "category": "Household", "itemName": "$total", "clicks": 60, "quantity": 12, "amount": 37 },
+          { "category": "Household", "itemName": "Hand wash", "clicks": 12, "quantity": 3, "amount": 15 },
+          { "category": "Household", "itemName": "Toothbrush", "clicks": 4, "quantity": 4, "amount": 12 },
+          { "category": "Household", "itemName": "Fairy dish liquid", "clicks": 44, "quantity": 5, "amount": 10 },
+         ];
+
+        expect(sortData(mockData, 'amount')).toEqual(expected);
+      });
     });
   });
 });
