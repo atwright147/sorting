d9c5f66cabc10e3ceea6ff81e80729dcd1ae09da
Andy Wright <atwright147@gmail.com>
2022-04-17 18:18:48 +0100

    make sorting for clicks work

--- src/sort.js
+++ src/sort.js
@@ -5,13 +5,26 @@ export const fetchData = async (url) => {
   return response.json();
 };
 
+// return Number(a[key]) - Number(b[key]) && (a.category.localeCompare(b.category) || a.itemName.localeCompare(b.itemName))
+
 export const sortData = (data, key = 'quantity') => {
   // Sort data here
 
   const sorted = data.sort(
-    (a, b) =>
-      Number(a[key]) - Number(b[key]) &&
-      ( a.category.localeCompare(b.category) || a.itemName.localeCompare(b.itemName) )
+    (a, b) => {
+      if (a.category > b.category) return 1
+      if (a.category < b.category) return -1
+
+      if (a.category === b.category) {
+        if (Number(a.clicks) > Number(b.clicks)) return -1
+        if (Number(a.clicks) < Number(b.clicks)) return 1
+      }
+
+      if (a.itemName > b.itemName) return 1
+      if (a.itemName < b.itemName) return -1
+
+      return 0
+    }
   );
 
   return sorted;
